80239db08451e50c60fbce1551a9bc1baadb7c0d
unknown <leogelsistemas@gmail.com>
2022-11-27 20:22:52 -0200

    config

--- static/js/adddiv.js
+++ static/js/adddiv.js
@@ -20,27 +20,29 @@ function decrement() {
 
 
 const form = document.getElementById('task-form');
-const taskList = document.getElementById('tasks');
+const taskList = document.getElementById('linha');
 const inputtype = document.getElementById('inputhidden');
 var criou = 0;
 var criado = 0;
 
 
 form.onsubmit = function (e) {
-    if (criou <= 4){
+
 	e.preventDefault();
 	const inputField = document.getElementById('task-input');
 	addTask(inputField.value);
 	form.reset();
 };
-}
+
 function addTask(description) {
 
 
 	const taskContainer = document.createElement('div');
 	const taskSubmit = document.createElement('div')
 	const newTask = document.createElement('input');
-	const taskLabel = document.createElement('label');
+
+	//const taskLabel = document.createElement('label');
+
 	editType = document.getElementById('inputhidden');
 	criarContagem = document.getElementById('inputcontagem');
 
@@ -51,10 +53,22 @@ function addTask(description) {
 
 
 
-	const taskDescriptionNode = document.createTextNode(description);
-	newTask.setAttribute('type', 'text');
+	const taskDescriptionNode = document.createTextNode('Autor:');
+    if (criou <= 3){
+    newTask.setAttribute('type', 'text');
+    };
+
+	if (criou >= 4){
+	newTask.setAttribute('type', 'hidden');
+	};
 	newTask.setAttribute('name', criou);
 	newTask.setAttribute('id', description);
+	newTask.setAttribute('value', description);
+	newTask.setAttribute('class', 'form-control mt-2 ');
+	taskContainer.setAttribute('class', 'text-center')
+	if (criou >= 4){
+	newTask.setAttribute('value', '');
+	};
 	newTask.setAttribute('value', description);
 	editType.setAttribute('type', 'hidden');
 	//criarContagem.setAttribute('type', 'text');
@@ -62,19 +76,22 @@ function addTask(description) {
 	criarContagem.setAttribute('name', criou);
 
 	criou = criou + 1;
-	criarElemento.setAttribute('type', 'submit');
-	criarElemento.setAttribute('value', 'submit');
+	//criarElemento.setAttribute('type', 'submit');
+	//criarElemento.setAttribute('value', 'submit');
 
 
 	//taskLabel.setAttribute('for', description);
+	//taskLabel.setAttribute('class', 'mt-2 text-center');
+
 	//taskLabel.appendChild(taskDescriptionNode);
 
 	taskContainer.classList.add('autores');
+	//taskContainer.appendChild(taskLabel);
 	taskContainer.appendChild(newTask);
-	taskContainer.appendChild(taskLabel);
 
-    taskSubmit.classList.add('task-submit');
-	taskSubmit.appendChild(criarElemento);
+
+    //taskSubmit.classList.add('task-submit');
+	//taskSubmit.appendChild(criarElemento);
 
 	taskList.appendChild(taskContainer);
 	taskList.appendChild(taskSubmit);
