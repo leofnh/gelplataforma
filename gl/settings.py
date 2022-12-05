dc3f10f727debadd64b9a2fbcf0343dfc416da7b
unknown <leogelsistemas@gmail.com>
2022-12-05 19:42:51 -0200

    config

--- gl/settings.py
+++ gl/settings.py
@@ -11,8 +11,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-4dy!3ksc&cz#t8_@!)5-9-^=jpb(vg+fj6e4_my=053kuzois1'
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'django-insecure-4dy!3ksc&cz#t8_@!)5-9-^=jpb(vg+fj6e4_my=053kuzois1'
+#SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
